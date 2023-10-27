5fd5e10c918c49fc3055c12648fdb6a99bc3577b
zoeNNN03 <126237482+zoeNNN03@users.noreply.github.com>
2023-10-28 05:18:03 +0700

    Update update.sql

--- update_delete/update.sql
+++ update_delete/update.sql
@@ -1,8 +1,6 @@
 PRAGMA foreign_keys = ON; -- enable foreign key
 PRAGMA encoding='UTF-8'; -- set encoding to UTF-8
 .print "Update testing"
--- set up database
-.read setup.sql
 
 -- show foreign key test of update
 SELECT * FROM contactStore;
@@ -10,5 +8,8 @@ UPDATE store SET store_id = 4 -- update store_id 1 to 4
 WHERE store_name = 'BUUshop test'; -- update store_name 'BUUshop test' to 'BUUshop'
 .print "Updated"
 SELECT * FROM contactStore; -- check result
+
+-- set up database
+.read setup.sql
 
 -- Path: update_delete/update.sql
